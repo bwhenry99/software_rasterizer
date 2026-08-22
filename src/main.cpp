48dcfdf86553fb75e5a261b3e465e1d2d30be5b1
bwhenry99 <bwhenry19@gmail.com>
2026-08-21 20:06:24 -0600

    initial window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+#include "raylib.h"
+
 int main()
 {
-	std::cout << "Hello World!";
-	return 0;
+   const int viewHeight = 512;
+   const int viewWidth = 512;
+   std::string title = "Software Rendered BAYBEE!!!";
+
+   InitWindow(viewWidth, viewHeight, title.c_str());
+
+   while (!WindowShouldClose())
+   {
+      BeginDrawing();
+      {
+         ClearBackground(RAYWHITE);
+         DrawText("Hello World", 128, 126, 32, BLACK);
+         
+      }
+      EndDrawing();
+
+
+   }
 }
