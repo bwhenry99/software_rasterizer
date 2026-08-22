bb0960fa4cce374dd90b15d6c45cc5ec40e5fdd0
bwhenry99 <bwhenry19@gmail.com>
2026-08-21 21:55:11 -0600

    drawing and pulsing blue for some reason

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,36 @@
 
 int main()
 {
-   const int viewHeight = 512;
-   const int viewWidth = 512;
+   const int viewHeight = 720;
+   const int viewWidth = 1080;
    std::string title = "Software Rendered BAYBEE!!!";
 
+   SetTargetFPS(60);
    InitWindow(viewWidth, viewHeight, title.c_str());
-
+   Color*  frameBuffer = new Color[viewHeight * viewWidth];
+   Image screenImage{ frameBuffer, viewWidth, viewHeight, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 };
+   Texture2D screen = LoadTextureFromImage(screenImage);
+   uint8_t globalAlpha = 255;
    while (!WindowShouldClose())
    {
+      globalAlpha--;
+      for (unsigned int i = 0; i < viewWidth; i++)
+      {
+         for (unsigned int j = 0; j < viewHeight; j++)
+         {
+            
+            unsigned int index = i + j * viewWidth;
+            frameBuffer[index].r = static_cast<uint8_t>(255 * i / viewWidth);
+            frameBuffer[index].g = static_cast<uint8_t>(255 * j / viewHeight);
+            frameBuffer[index].b = globalAlpha;
+            frameBuffer[index].a = globalAlpha;
+            
+         }
+      }
+      UpdateTexture(screen, frameBuffer);
       BeginDrawing();
       {
-         ClearBackground(RAYWHITE);
+         DrawTexture(screen, 0, 0, RAYWHITE);
          DrawText("Hello World", 128, 126, 32, BLACK);
          
       }
@@ -22,4 +41,9 @@ int main()
 
 
    }
+   UnloadTexture(screen);
+   delete frameBuffer;
+   CloseWindow();
+
+   return 0;
 }
